cfdae3f0b1b45d57f576452ef426083e7217d1fa
everton_goncalves <everton.goncalves.89@gmail.com>
2025-07-09 18:27:12 -0300

    feat: setting state using current state with hooks - useReducer

--- src/components/ProductHook/ProductHook.jsx
+++ src/components/ProductHook/ProductHook.jsx
@@ -1,46 +1,89 @@
-import React, { useState } from 'react';
+/* eslint-disable no-case-declarations */
+import React, { useReducer } from 'react';
 import './ProductHook.css';
+
+const products = [
+    {
+      emoji: '🍦',
+      name: 'ice cream',
+      price: 5
+    },
+    {
+      emoji: '🍩',
+      name: 'donuts',
+      price: 2.5,
+    },
+    {
+      emoji: '🍉',
+      name: 'watermelon',
+      price: 4
+    }
+  ];
 
 const currencyOptions = {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
 }
 
-function getTotal(total) {
+function getTotal(cart) {
+    const total = cart.reduce((totalCost, item) => totalCost + item.price, 0);
+
     return total.toLocaleString(undefined, currencyOptions)
 }
 
-export default function ProductHook() {
-    // > stateful data
-    // useState is a function that takes the initial state as an argument and returns an array with two items.
-    // The first item is the current state, and the second item is a function that updates the state.
-    // By passing an empty array as an argument to useState, you set the initial empty state as the first value of cart.
-    const [cart, setCart] = useState([]);
-    // The second useState is for the total, which is initialized to 0.
-    const [total, setTotal] = useState(0);
-
-    function add() {
-        setCart(['ice cream']);
-        setTotal(5);
+function cartReducer(state, action) {
+    switch(action.type) {
+      case 'add':
+        return [...state, action.product];
+      case 'remove':
+        const productIndex = state.findIndex(item => item.name === action.product.name);
+        if(productIndex < 0) {
+          return state;
+        }
+        const update = [...state];
+        update.splice(productIndex, 1)
+        return update
+      default:
+        return state;
     }
+  }
+
+export default function ProductHook() {
+    /*
+        The useReducer Hook is similar to useState, but when you initialize the Hook,
+        you pass in a function the Hook will run when you change the state along with
+        the initial data. The function—referred to as the reducer—takes two arguments: 
+        the state and another argument.
+        The other argument is what you will supply when you call the update function.
+    */
+    const [cart, setCart] = useReducer(cartReducer, []);
+
+    function add(product) {
+        setCart({ product, type: 'add' });
+    }
+
+    function remove(product) {
+        setCart({ product, type: 'remove' });
+      } 
 
     return(
         <div className="product-hook-wrapper">
             <div>
                 Shopping Cart: {cart.length} total items.
             </div>
-            <div>Total: {getTotal(total)}</div>
+            <div>Total: {getTotal(cart)}</div>
 
-            <div className="product-hook"><span role="img" aria-label="ice cream">🍦</span></div>
-            <button onClick={add}>Add</button>
-            <button
-                onClick={() => {
-                setCart([]);
-                setTotal(0);
-                }}
-            >
-                Remove
-            </button>
+            <div>
+            {products.map(product => (
+                <div key={product.name}>
+                    <div className="product-hook">
+                        <span role="img" aria-label={product.name}>{product.emoji}</span>
+                    </div>
+                    <button onClick={() => add(product)}>Add</button>
+                    <button onClick={() => remove(product)}>Remove</button>
+                </div>
+                ))}
+            </div>
         </div>
     )
 }
