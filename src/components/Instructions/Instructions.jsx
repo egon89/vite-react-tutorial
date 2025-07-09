a15d00fc17af8c5a9afce79090ca0bbb5bb38dec
everton_goncalves <everton.goncalves.89@gmail.com>
2025-07-09 11:36:37 -0300

    feat: building a functional component

--- src/components/Instructions/Instructions.jsx
+++ src/components/Instructions/Instructions.jsx
@@ -1,14 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import emoji from './assets/emoji.svg';
 import './Instructions.css'
 
-export default class Instructions extends Component {
-    render() {
-        return(
-          <div className="instructions">
-            <img alt="laughing crying emoji" src={emoji} />
-            <p>Click on an emoji to view the emoji short name.</p>
-          </div>
-        );
-      }
+export default function Instructions() {
+  return(
+    <div className="instructions">
+        <img alt="laughing crying emoji" src={emoji} />
+        <p>Click on an emoji to view the emoji short name.</p>
+    </div>
+  )
 }
+
+/*
+const Instructions = () => (
+  <div className="instructions">
+    ...
+  </div>
+)
+
+export default Instructions;
+*/
