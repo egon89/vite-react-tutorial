11d3f7ab80b946f5ed4c1deafe80bec9ddd1509b
everton_goncalves <everton.goncalves.89@gmail.com>
2025-07-09 10:43:21 -0300

    feat: events

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const displayEmojiName = event => alert(event.target.id);
+
 function App() {
-  const [count, setCount] = useState(0)
+  const greeting = "greeting"
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <h1 id={greeting}>Hello, World</h1>
+        <p>I am writing JSX</p>
+        <ul>
+          <li>
+            <button
+              onClick={displayEmojiName}
+            >
+              <span role="img" aria-label="grinning face" id="grinning face">😀</span>
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={displayEmojiName}
+            >
+                <span role="img" aria-label="party popper" id="party popper">🎉</span>
+            </button>
+          </li>
+          <li>
+              <button
+                onClick={displayEmojiName}
+              >
+                <span role="img" aria-label="woman dancing" id="woman dancing">💃</span>
+            </button>
+          </li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
